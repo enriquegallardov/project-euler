6e57707b8d5c72394e56242acb265a0dda6f62e3
Enrique Gallardo <62443583+enriquegallardov@users.noreply.github.com>
2022-06-24 16:17:29 -0500

    problem 3 import fix

--- solutions/problem_003.py
+++ solutions/problem_003.py
@@ -5,9 +5,6 @@ factor of the number 600851475143 ?
 """
 
 
-from math import sqrt
-
-
 def largest_prime_factor(n):
     if n % 2 == 0:
         last_factor = 2
@@ -18,14 +15,14 @@ def largest_prime_factor(n):
         last_factor = 1
 
     factor = 3
-    max_factor = sqrt(n)
+    max_factor = n ** 0.5
     while n > 1 and factor <= max_factor:
         if n % factor == 0:
             n /= factor
             last_factor = factor
             while n % factor == 0:
                 n /= factor
-            max_factor = sqrt(n)
+            max_factor = n ** 0.5
         factor += 2
     if n == 1:
         return int(last_factor)
